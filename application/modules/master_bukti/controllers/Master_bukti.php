aadfc8955d7c372a7e2bde608191e05f2f38cacb
andreantama <andreanyogatama1@gmail.com>
2020-11-23 06:22:01 +0700

    tambah fitur master bukti transfer

---
+++ application/modules/master_bukti/controllers/Master_bukti.php
@@ -0,0 +1,52 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Master_bukti extends MY_Admin {
+
+	var $template = 'admin_page';
+
+	function __construct(){
+		parent::__construct();
+		$this->load->model('M_master_bukti','master_buktipendaftaran');
+    }
+    public function index()
+    {
+        $data['judul_page'] = 'Bukti Pendaftaran';
+		$data['des_page'] = '';
+		$data['page'] = 'index';
+        $data['modul_active'] = 'master_buktipendaftaran';
+        $sqlWhere = array();
+		$data['active'] = $this->master_buktipendaftaran->tampilData('tbl_pmbbuktitransfer','*',$sqlWhere, TRUE);
+		$this->load->view($this->template,$data);
+    }
+    public function getData()
+    {
+        if($this->input->is_ajax_request()){
+            $this->load->library('jariprom_tools');
+            $this->master_buktipendaftaran->setTableDatabase('tbl_pmbbuktitransfer');
+			$this->master_buktipendaftaran->setSelectColumn(array('ID','NAMA','BERKAS'));
+			$this->master_buktipendaftaran->setOrderColumn(array('NAMA',NULL,NULL,NULL));
+			$this->master_buktipendaftaran->setOrderId(array('ID','DESC'));
+			$this->master_buktipendaftaran->setSearchQuery(array('NAMA'));
+			$sqlWhere = array();
+			$this->master_buktipendaftaran->setWhereColumn($sqlWhere);
+			$fetch_data = $this->master_buktipendaftaran->generateDatatables();
+			$data = array();
+			foreach($fetch_data as $row){
+	            $sub_array = array();
+	            $sub_array[] = $row->NAMA;
+	            $sub_array[] = '<img src='.base_url("assets/buktitransfer/".$row->BERKAS).' class="img-thumbnail">';
+	            $data[] = $sub_array;
+	       }
+	       $output = array(
+	            "draw" => intval($_GET["draw"]),
+	            "recordsTotal" => $this->master_buktipendaftaran->get_all_data(),
+	            "recordsFiltered" => $this->master_buktipendaftaran->get_filtered_data(),
+	            "data" => $data
+	       );
+	       return $this->output->set_content_type('application/json')->set_output(json_encode($output));
+		}
+		else{
+			echo 'No direct script access allowed.';
+		}
+    }
+}
---
+++ application/modules/master_bukti/views/index.php
@@ -0,0 +1,102 @@
+<div class="row">
+<div class="col-md-12">
+
+<div class="panel panel-default">
+
+	<div class="panel-body">
+
+	<?php
+
+		if($this->session->flashdata('notif')):
+
+	?>
+
+	<div class="callout callout-<?= $this->session->flashdata('clr') ?>">
+
+		<?= $this->session->flashdata('notif') ?>
+
+	</div>
+
+	<?php endif; ?>
+		<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+
+        <thead>
+
+            <tr>
+
+                <th>Nama</th>
+
+                <th>Berkas</th>
+				<th></th>
+				<th></th>
+
+            </tr>
+
+        </thead>
+
+    </table>
+
+	</div>
+
+</div>
+
+	</div>
+</div>
+<script>
+
+$(document).ready(function() {
+	$('#idSearchStart').datepicker({
+
+		autoclose: true,
+
+		format: 'yyyy-mm-dd'
+
+	});
+	$('#idSearchEnd').datepicker({
+
+autoclose: true,
+
+format: 'yyyy-mm-dd'
+
+});
+var base_url = "<?php echo base_url("master_bukti/getData") ?>";
+
+$('#example').dataTable({
+
+	"processing": true,
+
+    "serverSide": true,
+
+    "order":[],
+
+    "ajax":{  
+
+	    url: base_url
+
+	},
+
+	"columnDefs":[
+
+	{
+
+		"targets":[1,2,3],
+
+		"orderable":false,  
+
+    },
+
+   ],
+
+   "aLengthMenu": [[5, 10, 15, 20, 25], [5, 10, 15, 20, 25]],
+
+   "iDisplayLength": 5,
+
+   "bInfo": false,
+
+   "bAutoWidth": false 
+
+   });
+
+});
+
+    </script>
